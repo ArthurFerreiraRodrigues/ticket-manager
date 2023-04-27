4bb57b1d1bde51706bf1ab959e80294c7de44cb6
Arthur <arthur.250402@gmail.com>
2023-04-26 21:19:42 -0300

    Sobe base

---
+++ src/model/Passagem.java
@@ -0,0 +1,8 @@
+package model;
+
+public abstract class Passagem {
+    private String nomePassageiro;
+    private String telefone;
+    private Intinerario intinerario;
+    private String asssento;
+}
